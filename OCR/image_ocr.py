cfe119d1dfb981087ac1aa8a4b9e1ee851e54eb8
Tooba_Rani <toobarani708@gmail.com>
2026-02-27 14:17:16 +0500

    Change API key for OCR processing
    Updated API key to a new value without an expiration date.

--- OCR/image_ocr.py
+++ OCR/image_ocr.py
@@ -5,8 +5,8 @@ from mistralai import Mistral
 
 # This function encapsulates the OCR logic to be called from the UI.
 def process_ocr_from_image_bytes(image_bytes):
-    # expire date is 31 december 2025
-    API = "QS2s8StY5FAdMBhfRJZ7zP5AXPLzGmkF"
+    # no expire date
+    API = "YNknqcBe6YgaTqPOkmiOFpIHrHeZviqc"
     client = Mistral(api_key=API)
 
     # Encode the image to base64
